41f8370819427438dd1ce8b4110e1eb5e9870043
mathiuzdev <matias.pedro.ortega@gmail.com>
2024-03-06 23:16:58 -0300

    Router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App.tsx'
+
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Contact } from './pages/Contact.tsx';
+import { Home } from './pages/Home.tsx';
+import { Tasar } from './pages/Tasar.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path='/contacto' element={<Contact />} />
+        <Route path='/tasaciones' element={<Tasar />} />
+      </Route>
+  )
+);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
