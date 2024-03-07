d0c79499a6b1a46a9615ac15d5317f41e1400ad1
mathiuzdev <matias.pedro.ortega@gmail.com>
2024-03-06 23:16:09 -0300

    Props modified

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
+
 import { Nav } from "./Nav";
 // import image from '/public/images/jesica-ortiz-inmobiliaria.svg';
 import image from '/images/logo-inmobiliaria.png';
-interface listaNav{
-  lista:string[]
-}
-export const Header = ({lista}:listaNav) => {
+
+
+export const Header = () => {
   return (
-    <header className="flex h-24 bg-colorLogo fixed w-full shadow-lg z-50 ">
+    <header className="flex h-24 bg-colorLogo fixed w-full shadow-lg z-50">
         <div className="w-1/4">
             <img className="h-24 pl-2 " src={image} alt="" />
         </div>
-        <Nav lista={lista} />
+        <Nav />
     </header>
   )
 }
