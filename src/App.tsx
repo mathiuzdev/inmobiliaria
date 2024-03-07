4ce2468a46777780bc953b9f23346e271596696f
mathiuzdev <matias.pedro.ortega@gmail.com>
2024-03-06 23:19:27 -0300

    Component App deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { Contact } from "./components/Contact"
-import { Footer } from "./components/Footer"
-import { Header } from "./components/Header"
-import { Main } from "./components/Main"
-import { Tasar } from "./components/Tasar"
+import { Outlet } from "react-router"
 
 export const App = () => {
-  const listaNav = ["COMPRAR","ALQUILAR","TASÁ AHORA","CONTACTO"];
-
   return (
-    <>
-      <Header lista={listaNav}/>
-      {/* <Main className="w-3/4 flex mt-32" children={<Contact></Contact>} /> */}
-      <Main className="w-full flex mt-32" children={<Tasar></Tasar>} />
-      <Footer lista={listaNav}/>
-
-
-
-    </>
+    <Outlet/>
   )
 }
