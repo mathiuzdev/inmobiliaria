4717cb2d4b36c00b193b3e06acd03ba3816f1da5
mathiuzdev <matias.pedro.ortega@gmail.com>
2024-03-08 20:37:36 -0300

    Delete a comment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 import image from "/images/logo-inmobiliaria.png";
 import CopyrightIcon from "@mui/icons-material/Copyright";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
-import PhoneIcon from "@mui/icons-material/Phone";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+// import MailOutlineIcon from "@mui/icons-material/MailOutline";
+// import PhoneIcon from "@mui/icons-material/Phone";
+// import LocationOnIcon from "@mui/icons-material/LocationOn";
 import listaNav from "../helpers/listasDinamicas";
 
 const lista = listaNav();
@@ -23,45 +23,10 @@ export const Footer = () => {
             key={list.title}
             className="font-bold flex h-fit justify-center my-3 mx-1 px-4"
           >
-            {" "}
             {list.title}
           </li>
         ))}
       </ul>
-
-      {/* <div className="flex justify-center  pb-5 space-x-12">
-       */}
-      {/* <div className="flex items-center">
-          <span className="flex justify-center pr-3">
-            <PhoneIcon sx={{ fontSize: 40 }} />
-          </span>
-          <p className="font-semibold" >+54 9 2262-240370</p>
-        </div>
-        <div className="flex items-center">
-          <span className="flex justify-center pr-3">
-            <MailOutlineIcon sx={{ fontSize: 40 }} />
-          </span>
-          <p className="font-semibold"> jesicaortizestudioinmobiliario@gmail.com</p>
-        </div>
-        <div className="flex items-center">
-          <span className="flex justify-center pr-2">
-            <LocationOnIcon sx={{ fontSize: 40 }} />
-          </span>
-          <p className="font-semibold"> 70 3026</p>
-        </div> */}
-      {/* </div> */}
-      {/* <ul className="flex justify-center">
-        {lista.map((title) => (
-          <li
-            key={title}
-            className="font-bold flex h-fit justify-center my-3 mx-1 px-4"
-          >
-            {" "}
-            {title}
-          </li>
-        ))}
-      </ul> */}
-
       <div className="flex justify-center items-center">
         <p className="font-semibold h-full flex items-center">
           <span className="pr-2 items-center">
