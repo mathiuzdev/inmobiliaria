46f09efe7d8ab6e4ed600d53546a93613b916e60
mathiuzdev <matias.pedro.ortega@gmail.com>
2024-03-11 00:08:06 -0300

    Change names and class

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -2,7 +2,7 @@
 import { Card, CardBody, CardFooter } from "@nextui-org/card"
 import { Image } from "@nextui-org/image"
 
-import ReactImageGallery from "react-image-gallery";
+import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 
 export const HomeCard = () => {
@@ -25,16 +25,16 @@ export const HomeCard = () => {
   return (
     <Card className="pt-[7px] pb-10 rounded-lg bg-colorLogo h-auto grow-0 desktop:w-[23.6%] tablet:w-[48%] notebook:w-[31.6%] laptop:w-[48%] phone:w-full">
       <CardBody className="py-1 w-full overflow-hidden">
-        <div className="relative h-[200px] rounded-lg">
+        <div className="flex w-full relative h-[200px]">
 
 
 
-          <ReactImageGallery items={images} autoPlay={true} showBullets={false} renderItem={(item => <Image
+          <ImageGallery items={images} autoPlay={true} showBullets={false} slideDuration={1500} slideInterval={6000} renderItem={(item => <Image
             alt="Card background"
-            className="rounded-sm object-cover object-center w-full h-full"
+            className="object-cover object-center w-full h-full rounded-none"
             src={item.original}
             removeWrapper
-          />)} additionalClass="botones" showPlayButton={false} showFullscreenButton={false} showThumbnails={false} />
+          />)} additionalClass="botones h-full" showPlayButton={false} showFullscreenButton={false} showThumbnails={false} />
           <p className="absolute top-0 right-0 z-10 bg-black p-1 w-[110px] text-white text-center text-sm">
             VENTA
           </p>
