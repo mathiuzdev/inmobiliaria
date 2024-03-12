9664701e3f2cfec952034fe9503a78b30f0ca1e9
Luna Rodriguez <luna.candela.rodriguez@gmail.com>
2024-03-12 00:19:38 -0300

    index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,40 +1,28 @@
-import { Layout } from "../components/Layout"
-import { Footer } from "../components/Footer"
-import { Header } from "../components/Header"
-import { Main } from "../components/Main"
-
-
-
+import { Layout } from "../components/Layout";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Main } from "../components/Main";
+import { Image } from "@nextui-org/react";
 
 export const Index = () => {
-    return (
-        <>
-            {/* <div className="w-full min-h-screen flex flex-col justify-between">           
-             <div className="bg-red-500"><p>Hola</p></div>
-                <div className="bg-green-500"><p>Hola</p></div>
-                <div className="bg-blue-500"><p>Hola</p></div>
-            </div>
- */}
+  return (
+    <>
+      <Layout clases="w-full min-h-screen flex flex-col justify-between">
+        <Header />
 
-            <Layout clases="w-full min-h-screen flex flex-col justify-between">
-
-                <Header />
-
-                <Main clases="w-3/4 flex mt-24">
-                    <div className="w-3/4 h-full bg-white rounded-2xl  m-2 px-4 py-4 text-xl">
-                        <div className="w-full py-4">
-                            <h1 className="py-3 font-bold">SOY EL INICIO</h1>
-                        </div>
-                        <div className="w-full flex h-72">
-                         
-                        </div>
-                    </div>
-                </Main>
-                <Footer />
-            </Layout>
-
-
-
-        </>
-    )
-}
+        <Main clases="flex mt-24 w-full">
+        <div className="flex flex-col items-center justify-center w-full">
+    <div className="w-full h-[480px] bg-green-600 relative">
+        <img src="images/index.jpg" alt="" className="object-cover w-full h-full" />
+        <div className="absolute inset-0 flex items-center justify-center bo">
+            <p className=" text-[50px] font-bold">Encontra tu propiedad</p>
+        </div>
+    </div>
+    <div className="bg-blue-500">hola</div>
+</div>
+        </Main>
+        <Footer />
+      </Layout>
+    </>
+  );
+};
